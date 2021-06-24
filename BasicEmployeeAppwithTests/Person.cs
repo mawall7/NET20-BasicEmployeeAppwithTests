00157f3c16fc66ce75dbaac805562ee8a5685909
Mattias Wallerman <wallermanmattias@yahoo.se>
2021-06-24 17:18:50 +0200

    Add project files.

---
+++ BasicEmployeeAppwithTests/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicEmployeeAppwithTests
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Salary { get; set; }
+    }
+}
---
+++ BasicEmployeeAppwithTests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicEmployeeAppwithTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
